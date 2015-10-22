b683a1da4991b900eb7e240a48316c5da1c19eba
Jack Wearden <jack@jackwearden.co.uk>
2015-10-22 20:09:50 +0100

    Toggleable optimisation

--- src/compiler.ml
+++ src/compiler.ml
@@ -14,12 +14,14 @@ let string_of_position lexbuf =
   let col = string_of_int (pos.pos_cnum - pos.pos_bol) in
   "Line "^line^", Column "^col
 
-let parse lexbuf =
+let parse' lexbuf =
   try Parse (Parser.top Lexer.read lexbuf) with
   | Lexer.SyntaxError msg ->  SyntaxError (string_of_position lexbuf)
   | Parser.Error ->           ParseError (string_of_position lexbuf)
 
-let eval s = parse (Lexing.from_string s)
+let parse s = parse' (Lexing.from_string s)
+
+let optimise ast = ast
 
 let prettify p =
   String.concat " " (List.map Js.string_of_statement p)
--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,15 @@
+open Arg
+open List
+open String
+
+let optimisation = ref false
+let param_specs = [
+  ("-o", Arg.Set optimisation, "Enable Optimisation")
+]
+let drop x = ()
+let usage_msg = "main.native [-o]"
+let _ = Arg.parse param_specs drop usage_msg
+
 let rec read_to_empty buf =
   let s = read_line () in
     if s = "" then buf
@@ -5,11 +17,14 @@ let rec read_to_empty buf =
           Buffer.add_string buf "\n";
           read_to_empty buf)
 
-let eval_from_input =
-  Compiler.eval (Buffer.contents (read_to_empty (Buffer.create 1)))
+let load_from_stdio = Buffer.contents (read_to_empty (Buffer.create 1))
+
+let compiler_chain =  let input = load_from_stdio in
+                      let ast = Compiler.parse input in
+                      if !optimisation then Compiler.optimise ast else ast
 
 let _ =
-  match eval_from_input with
+  match compiler_chain with
     | Compiler.Parse ss -> Compiler.prettyPrint ss
     | Compiler.SyntaxError msg -> print_endline ("Syntax Error: " ^ msg)
     | Compiler.ParseError msg -> print_endline ("Parse Error: " ^ msg)
