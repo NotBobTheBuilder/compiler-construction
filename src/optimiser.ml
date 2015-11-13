a6da73a21f215aff7667d2567b93dc9ea561ecdd
Jack Wearden <jack@jackwearden.co.uk>
2015-11-13 02:29:18 +0000

    Constant propagation p1

--- src/optimiser.ml
+++ src/optimiser.ml
@@ -3,49 +3,53 @@ open Js
 open String
 module StringMap = Map.Make(String)
 
-let rec fold_expr = function
-  | Add (lhs, rhs) ->   let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+let rec fold_expr scope = function
+  | Add (lhs, rhs) ->   let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> Number (n1+n2)
                           | _ -> Add (fl, fr))
-  | Sub (lhs, rhs) ->   let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | Sub (lhs, rhs) ->   let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> Number (n1-n2)
                           | _ -> Sub (fl, fr))
-  | Mul (lhs, rhs) ->   let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | Mul (lhs, rhs) ->   let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> Number (n1*n2)
                           | _ -> Mul (fl, fr))
-  | Lt (lhs, rhs) ->    let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | Lt (lhs, rhs) ->    let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> if n1 < n2 then True else False
                           | _ -> Lt (fl, fr))
-  | LtEq (lhs, rhs) ->  let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | LtEq (lhs, rhs) ->  let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> if n1 <= n2 then True else False
                           | _ -> Lt (fl, fr))
-  | Gt (lhs, rhs) ->    let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | Gt (lhs, rhs) ->    let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> if n1 > n2 then True else False
                           | _ -> Lt (fl, fr))
-  | GtEq (lhs, rhs) ->  let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | GtEq (lhs, rhs) ->  let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> if n1 >= n2 then True else False
                           | _ -> Lt (fl, fr))
-  | Eq (lhs, rhs) ->    let fl = fold_expr lhs in
-                        let fr = fold_expr rhs in
+  | Eq (lhs, rhs) ->    let fl = fold_expr scope lhs in
+                        let fr = fold_expr scope rhs in
                         (match (fl, fr) with
                           | (Number n1, Number n2) -> if n1 == n2 then True else False
                           | _ -> Lt (fl, fr))
   | Function (n, ps, scope, b) -> let (s, b) = fold_block b in Function (n, ps, s, b)
-  | Call (name, es) ->      let fs = List.map fold_expr es in Call (name, fs)
+  | Call (name, es) ->      let fs = List.map (fold_expr scope) es in Call (name, fs)
+  | Ident a -> (if StringMap.mem a scope then (match StringMap.find a scope with
+      | Some n -> Number n
+      | None -> Ident a)
+    else Ident a)
   | a -> a
 
 and is_truthy = function
@@ -58,16 +62,16 @@ and is_falsy = function
   | Number 0 -> true
   | _ -> false
 
-and fold_while scope c ss =       let fc = fold_expr c in
+and fold_while scope c ss =       let fc = fold_expr scope c in
                                   if is_falsy fc then []
                                   else let (_, b) = fold_block ss in [While (c, b)]
 
-and fold_if scope c ss =          let fc = fold_expr c in
+and fold_if scope c ss =          let fc = fold_expr scope c in
                                   if is_falsy fc then []
                                   else let (_, fs) = fold_block ss in
                                   if is_truthy fc then fs else [If (fc, fs)]
 
-and fold_if_else scope c ts fs =  let fc = fold_expr c in
+and fold_if_else scope c ts fs =  let fc = fold_expr scope c in
                                   let (_, ffs) = fold_block fs in
                                   let (_, fts) = fold_block ts in
                                   if is_falsy fc then ffs
@@ -75,18 +79,18 @@ and fold_if_else scope c ts fs =  let fc = fold_expr c in
                                   else [IfElse (fc, fts, ffs)]
 
 and fold_statement scope = function
-  | Return e -> [Return (fold_expr e)]
-  | Assign (i, e) -> [Assign (i, fold_expr e)]
-  | Declare (i, e) -> [Declare (i, fold_expr e)]
+  | Return e -> [Return (fold_expr scope e)]
+  | Assign (i, e) -> [Assign (i, fold_expr scope e)]
+  | Declare (i, e) -> [Declare (i, fold_expr scope e)]
   | While (c, ss) -> fold_while scope c ss
   | If (c, ts) -> fold_if scope c ts
   | IfElse (c, ts, fs) -> fold_if_else scope c ts fs
-  | Expr e -> [Expr (fold_expr e)]
+  | Expr e -> [Expr (fold_expr scope e)]
 
 and fold_block' scope statements = match statements with
   | [] -> []
   (* drop dead code after return *)
-  | (Return e)::tl -> [Return (fold_expr e)]
+  | (Return e)::tl -> [Return (fold_expr scope e)]
   | hd::tl -> List.append (fold_statement scope hd) (fold_block' scope tl)
 and fold_block statements = let (scope, stats) = hoist_consts statements in (scope, fold_block' scope stats)
 
